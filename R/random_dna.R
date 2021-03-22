35597d61ffbb347a1d08fd02de03a4e72d2562a2
LeneChr <s163628@student.dtu.dk>
2021-03-22 08:50:31 +0000

    Function for random DNA

---
+++ R/random_dna.R
@@ -0,0 +1,8 @@
+# R/random_dna.R
+library("devtools")
+
+random_dna <- function(l){
+  nucleotides <- sample(c("A", "T", "G", "C"), size = l, replace = TRUE)
+  dna = paste0(nucleotides, collapse = "")
+  return(dna)
+}
